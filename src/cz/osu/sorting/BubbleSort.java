a69abd491af1a0ecb5fbc61119642096b7497193
David Miko <ph4nt0mgh0sT17@gmail.com>
2019-12-05 18:55:17 +0100

    Added comments to methods.

--- src/cz/osu/sorting/BubbleSort.java
+++ src/cz/osu/sorting/BubbleSort.java
@@ -10,19 +10,38 @@ public class BubbleSort {
         internalSort(array);
     }
 
+    /**
+     * Internal method for sorting using bubblesort
+     * @param array The given int array.
+     */
     private static void internalSort(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             internalBubbleSortLoop(array,i);
         }
     }
 
+    /**
+     * Executes inner loop from Bubble Sort algorithm.
+     * @param array The integer array.
+     * @param currentIndex Current index from outer array.
+     */
     private static void internalBubbleSortLoop(int[] array, int currentIndex) {
         for (int j = 0; j < array.length - currentIndex - 1; j++) {
             if (array[j] > array[j+1]) {
-                int tempValue = array[j];
-                array[j] = array[j+1];
-                array[j+1] = tempValue;
+                swapArrayValues(array,j,j+1);
             }
         }
+    }
+
+    /**
+     * Swaps array values - First index with second index.
+     * @param array The given array where values will be swapped.
+     * @param firstIndex The index of first value.
+     * @param secondIndex The index of second value.
+     */
+    private static void swapArrayValues(int[] array, int firstIndex, int secondIndex) {
+        int tempValue = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = tempValue;
     }
 }
