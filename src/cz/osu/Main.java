0563d08877d55f5fcf398b2ce9288dfdf55dab61
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-01-11 14:35:27 +0100

    Added information to Main.java

--- src/cz/osu/Main.java
+++ src/cz/osu/Main.java
@@ -5,25 +5,33 @@ import cz.osu.sorting.*;
 import java.util.Random;
 
 public class Main {
+    final static int LENGTH = 25;
 
     public static void main(String[] args) throws Exception {
-        int[] array = new int[10];
+
+        int[] array = new int[LENGTH];
 
         Random rnd = new Random();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < LENGTH; i++) {
             array[i] = rnd.nextInt(1000);
         }
 
+        System.out.print("The array: " + printArray(array) + "\n");
+
         SelectionSort.sort(array);
 
-        for (int i : array) {
-            System.out.print(i + "; ");
+        System.out.print("The sorted array (Selection sort): " + printArray(array));
+    }
+
+    private static String printArray(int[] array) {
+        String report = "";
+
+        for (int number : array) {
+            report += number + "; ";
         }
 
-
-
-
+        return report;
     }
 
 }
