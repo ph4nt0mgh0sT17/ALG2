0f5b2d1d084d7a03a3e078f160d4f4536f172aba
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-01-11 03:12:43 +0100

    Used LinkedList in Main.java to demonstrate its uses.

--- src/cz/osu/Main.java
+++ src/cz/osu/Main.java
@@ -1,7 +1,17 @@
 package cz.osu;
 
+import cz.osu.structure.*;
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        Random random = new Random();
+        for (int i = 0; i < 50; i++) {
+            linkedList.add(random.nextInt(1000));
+        }
+
+        System.out.println(linkedList);
     }
 }
