835cc98e8d29dec0828683c1db1d3e9442cf6575
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-02-08 19:58:35 +0100

    Added regex and StringBuilder.

--- src/main/java/cz/osu/Main.java
+++ src/main/java/cz/osu/Main.java
@@ -1,10 +1,26 @@
 package cz.osu;
 
+import java.util.regex.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello from 'master' branch.");
+        Pattern pattern = Pattern.compile("(\\d{2})(\\d{2})(\\d{2})/(\\d+)");
+        Matcher mikoMatcher = pattern.matcher("990528/5973");
+
+        if (mikoMatcher.matches()) {
+            StringBuilder regexReport = new StringBuilder()
+                    .append("Year: 19")
+                    .append(mikoMatcher.group(1))
+                    .append("; Month: ")
+                    .append(mikoMatcher.group(2))
+                    .append("; Day: ")
+                    .append(mikoMatcher.group(3))
+                    .append("; Generated code: ")
+                    .append(mikoMatcher.group(4));
+
+            System.out.println(regexReport.toString());
+        }
     }
 
 }
