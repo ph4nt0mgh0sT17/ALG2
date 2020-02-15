3db53521a8ff98ff899f9d349f3893d2093f7ea6
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-02-16 00:10:07 +0100

    Updated a way how to create the welcome text.

--- src/main/java/cz/osu/Main.java
+++ src/main/java/cz/osu/Main.java
@@ -3,10 +3,18 @@ package cz.osu;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(getWelcomeText());
+    }
 
-        System.out.println("==================== 7ALG2 ====================\n\t\t\t17. 02. 2020\n\t\t\tDavid Miko\n===============================================");
-
-
+    /**
+     * Gets the welcome text in the console.
+     * @return The welcome text
+     */
+    private static String getWelcomeText() {
+        return "==================== 7ALG2 ====================\n" +
+               "|                17. 02. 2020                 |\n" +
+               "|                 David Miko                  |\n" +
+               "===============================================\n";
     }
 
 }
