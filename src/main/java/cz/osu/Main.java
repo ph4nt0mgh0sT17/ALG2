4d5c5875613f77890bc5754a1acee3f7c864a95c
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-02-16 00:13:18 +0100

    Added comment and method for generating welcome text.

--- src/main/java/cz/osu/Main.java
+++ src/main/java/cz/osu/Main.java
@@ -2,9 +2,14 @@ package cz.osu;
 
 public class Main {
 
+    /**
+     * Main entry of the console application.
+     * @param args Possible arguments passed by executing the java application by terminal or command line.
+     */
     public static void main(String[] args) {
 
-        System.out.println("==================== 7ALG2 ====================\n\t\t\t10. 02. 2020\n\t\t\tDavid Miko\n===============================================");
+        // Prints the welcome text
+        System.out.println(getWelcomeText());
 
         // Creates a bookshop
         Bookshop bookshop = new Bookshop(25);
@@ -22,6 +27,17 @@ public class Main {
         for (Book book : bookshop.getArrayListBooksUnderPrice(620)) {
             System.out.println(book);
         }
+    }
+
+    /**
+     * Gets the welcome text in the console.
+     * @return The welcome text
+     */
+    private static String getWelcomeText() {
+        return  "==================== 7ALG2 ====================\n" +
+                "|                10. 02. 2020                 |\n" +
+                "|                 David Miko                  |\n" +
+                "===============================================\n";
     }
 
 }
