69c047ca4088cdeffb8d0bb8dd2f3a0984236b6b
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-02-17 12:51:28 +0100

    Added explanation of some methods in DirectoryHandler.

--- src/main/java/cz/osu/DirectoryHandler.java
+++ src/main/java/cz/osu/DirectoryHandler.java
@@ -13,10 +13,20 @@ public class DirectoryHandler {
         return new File(pathname).exists();
     }
 
+    /**
+     * Creates a directory with given pathname.
+     * @param pathname The pathname of directory.
+     * @return The success of the creation.
+     */
     public boolean createDirectory(String pathname) {
         return new File(pathname).mkdir();
     }
 
+    /**
+     * Creates a directory with given pathname. If there are a few parent directories that do not exist then they will be created too.
+     * @param pathname The pathname of directory.
+     * @return The success of the creation.
+     */
     public boolean createDirectories(String pathname) {
         return new File(pathname).mkdirs();
     }
