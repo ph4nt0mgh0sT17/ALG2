dfd9b4dd00a04680b05c711d572215d02ba9c7fa
David Miko <ph4nt0mgh0sT17@gmail.com>
2020-02-21 20:37:04 +0100

    Test recursive.

--- src/main/java/cz/osu/Main.java
+++ src/main/java/cz/osu/Main.java
@@ -5,7 +5,7 @@ import java.util.regex.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Pattern pattern = Pattern.compile("(\\d{2})(\\d{2})(\\d{2})/(\\d+)");
+        /*Pattern pattern = Pattern.compile("(\\d{2})(\\d{2})(\\d{2})/(\\d+)");
         Matcher mikoMatcher = pattern.matcher("990528/5973");
 
         // Matcher must be checked if matches then groups can be found
@@ -21,7 +21,65 @@ public class Main {
                     .append(mikoMatcher.group(4));
 
             System.out.println(regexReport.toString());
+        }*/
+
+        int[] arrayNumbers = new int[]{
+                1,
+                -1000,
+                -50,
+                4,
+                10,
+                50
+        };
+
+        selectionSort(arrayNumbers);
+
+        for (int number : arrayNumbers) {
+            System.out.println(number);
         }
     }
 
+
+
+    private static int findMinimumIndex(int[] array, int currentIndex) {
+
+        return internalFindMinimumIndex(array,currentIndex, currentIndex);
+    }
+
+    private static int internalFindMinimumIndex(int[] array, int currentIndex, int minimumIndex) {
+        if (currentIndex < array.length - 1) {
+            if (array[currentIndex + 1] < array[currentIndex]) {
+                return internalFindMinimumIndex(array, currentIndex + 1, currentIndex + 1);
+            }
+
+            else {
+                return internalFindMinimumIndex(array, currentIndex + 1, minimumIndex);
+            }
+        }
+
+        return minimumIndex;
+    }
+
+    private static void selectionSort(int[] array) {
+        internalSelectionSort(array,0);
+    }
+
+    private static void internalSelectionSort(int[] array, int currentIndex) {
+        if (currentIndex < array.length - 1) {
+            int minimumIndex = findMinimumIndex(array,currentIndex);
+            swap(array, minimumIndex, currentIndex);
+
+            internalSelectionSort(array, currentIndex + 1);
+        }
+    }
+
+    private static void swap(int[] array, int left, int right) {
+        int tempValue = array[left];
+        array[left] = array[right];
+        array[right] = tempValue;
+    }
+
+
+
 }
+
